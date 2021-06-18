93bad8fcce4ae53ef08b6e706660519b486c52ea
Villafria <MV043156@cerner.net>
2021-06-18 16:20:50 -0500

    update query and files for Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
--- Retrieve retiring employees information and their titles
+-- Deliverable 1: Retrieve retiring employees information and their titles
 SELECT e.emp_no,
 e.first_name,
 e.last_name,
@@ -12,7 +12,7 @@ ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no ASC;
 
--- Remove duplicates rows of retiring employees and keep only the most recent title
+-- Deliverable 1: Remove duplicates rows of retiring employees and keep only the most recent title
 SELECT DISTINCT ON (rt.emp_no) 
 rt.emp_no,
 rt.first_name,
@@ -22,10 +22,29 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY rt.emp_no ASC, rt.to_date DESC;
 
---Retrieve number of employees by their most recent job title who are about to retire
+-- Deliverable 1: Retrieve number of employees by their most recent job title who are about to retire
 SELECT COUNT(ut.emp_no),
 ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC;
+
+--Deliverable 2: Retrieve employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON (e.emp_no) 
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
